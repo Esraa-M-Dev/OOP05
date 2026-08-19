f6575ac09209479f5dfe0977831bf7054317202c
Esraa Mohamed <esomo60@gmail.com>
2026-08-19 10:31:48 +0300

    Q(7):static class

---
+++ DeliveryUtilities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentOOP05
+{
+    internal static class DeliveryUtilities
+    {
+       public static void PrintSeperator()
+        {
+            Console.WriteLine("====================");
+        }
+        public static void PrintsystemTitle()
+        {
+            Console.WriteLine("Delivery center");
+        }
+    }
+}
